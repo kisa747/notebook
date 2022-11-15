895e489e15002bf5081f39188a3443e20a539232
lenovo-pc <kisa747@qq.com>
2022-11-16 02:13:48 +0800

    update

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 [![Documentation Status](https://readthedocs.org/projects/kisa747/badge/?version=latest)](https://kisa747.readthedocs.io/zh_CN/latest/?badge=latest)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
 
 # 概述
 
--- src/pyexcel/__init__.py
+++ src/pyexcel/__init__.py
@@ -5,10 +5,12 @@
 """
 __author__ = 'kevin'
 __date__ = '2021/2/3'
+
 try:
     from .__version__ import __version__, __version_tuple__
 except ImportError:
-    __version__, __version_tuple__ = '0.1.0', (0, 1, 0)
+    __version__ = version = '0.1.0'
+    __version_tuple__ = version_tuple = (0, 1, 0)
 from .converts import convert  # 转换 excel 文件
 from .excel import Excel
 from .utils import merge, split
